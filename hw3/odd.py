eb878ca9b84ac0130c062533580bc8553910e5ce
dombish <dombish@iu.edu>
2026-02-02 22:30:30 -0500

    forgot to remove a debug line of code from odd.py resulting in printing unneeded info

--- hw3/odd.py
+++ hw3/odd.py
@@ -1,7 +1,6 @@
 # program to generate a list of odd numbers from 1 to n, where n = 30 
 
 for num in range(1, 31):
-    print(num/2, int(num/2))
     # checks if dividing the number by 2 leaves a remainder, if it does the number is odd
     if num / 2 != int(num / 2):
         print(num)
